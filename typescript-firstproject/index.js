0213ff9a9bd9d4968a29b45f54e5708c4e3782a6
Gary Bleasdale <gary@bleasdale.dev>
2023-03-17 15:33:41 -0300

    added properties to property container

--- typescript-firstproject/index.js
+++ typescript-firstproject/index.js
@@ -1,4 +1,8 @@
+var returningUserDisplay = document.querySelector('#returning-user');
+var userNameDisplay = document.querySelector('#user');
 var reviewTotalDisplay = document.querySelector('#reviews');
+var isOpen;
+var propertyDashboard = document.querySelector('.properties');
 var reviews = [
     {
         name: 'Sheia',
@@ -19,17 +23,70 @@ var reviews = [
         date: '27-03-2021'
     },
 ];
-function showReviewTotal(value) {
+var properties = [
+    {
+        image: 'https://metro.co.uk/wp-content/uploads/2023/03/GettyImages-1309290300-d18c.jpg?quality=90&strip=all',
+        title: 'Colombian Shack',
+        price: 45,
+        location: {
+            firstLine: 'shack 37',
+            city: 'Bogota',
+            code: 45632,
+            country: 'Colombia'
+        },
+        contact: 'marywinkle@gmail.com',
+        available: true
+    },
+    {
+        image: 'https://upload.wikimedia.org/wikipedia/commons/c/ce/PA260057.JPG',
+        title: 'Polish Cottage',
+        price: 34,
+        location: {
+            firstLine: 'no 23',
+            city: 'Gdansk',
+            code: 343903,
+            country: 'Poland'
+        },
+        contact: 'garydavis@hotmail.com',
+        available: false
+    },
+    {
+        image: 'https://www.buyincolombia.com/wp-content/uploads/2018/08/buy-in-colombia-colombia-real-estate-realty-finca-raiz-colombia-lots-for-sale-lotes-en-venta-guatavita-gv-19-56-818x540.jpg',
+        title: 'London Flat',
+        price: 23,
+        location: {
+            firstLine: 'flat 15',
+            city: 'London',
+            code: 35433,
+            country: 'United Kingdom'
+        },
+        contact: 'andyluger@aol.com',
+        available: true
+    }
+];
+function showReviewTotal(value, reviews, loyaltyUser) {
     reviewTotalDisplay.innerHTML = 'review total ' + value.toString();
+    reviewTotalDisplay.innerHTML += " | last reviewed by: ".concat(reviews[0].name);
+    loyaltyUser ? reviewTotalDisplay.innerHTML += "&#9733" : reviewTotalDisplay.innerHTML += "";
 }
-showReviewTotal(reviews.length);
-function showMostRecentReviewerName(reviews) {
-    var mostRecentReviewerIndex;
-    var parsedDatesArray = reviews.map(function (element) {
-        var date = new Date(element.date);
-        return date;
+showReviewTotal(reviews.length, reviews, reviews[0].loyaltyUser);
+var you = {
+    firstName: 'Bobby',
+    lastName: 'Brown',
+    isReturning: true,
+    age: 35,
+    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
+};
+function populateUser(isReturning, userName) {
+    if (isReturning == true) {
+        returningUserDisplay.innerHTML = 'back';
+    }
+    userNameDisplay.innerHTML = userName;
+}
+populateUser(you.isReturning, you.firstName);
+var displayProperties = function (properties) {
+    properties.forEach(function (e) {
+        propertyDashboard.innerHTML += "<div class='single-property-container'><h2>".concat(e['title'], "</h2><img src=").concat(e['image'], " class='property-image'/></div>");
     });
-    console.log(parsedDatesArray);
-    reviewTotalDisplay.innerHTML += " ".concat(reviews[0].name);
-}
-showMostRecentReviewerName(reviews);
+};
+displayProperties(properties);
--- typescript-firstproject/index.ts
+++ typescript-firstproject/index.ts
@@ -1,6 +1,17 @@
+const returningUserDisplay = document.querySelector('#returning-user')
+const userNameDisplay = document.querySelector('#user')
 const reviewTotalDisplay = document.querySelector('#reviews')
+let isOpen: boolean
+const propertyDashboard = document.querySelector('.properties')
 
-const reviews = [
+
+
+const reviews : {
+    name: string;
+    stars:number;
+    loyaltyUser:boolean;
+    date: string
+}[] = [
     {
         name: 'Sheia',
         stars: 5,
@@ -21,15 +32,98 @@ const reviews = [
     },
 ]
 
+const properties : {
+    image: string;
+    title: string;
+    price: number;
+    location: {
+        firstLine: string,
+        city: string,
+        code: number,
+        country: string
+    },
+    contact: string;
+    available: boolean
+}[] = [
+    {
+        image: 'https://metro.co.uk/wp-content/uploads/2023/03/GettyImages-1309290300-d18c.jpg?quality=90&strip=all',
+        title: 'Colombian Shack',
+        price: 45,
+        location: {
+            firstLine: 'shack 37',
+            city: 'Bogota',
+            code: 45632,
+            country: 'Colombia'
+        },
+        contact: 'marywinkle@gmail.com',
+        available: true  
+    },
+    {
+        image: 'https://upload.wikimedia.org/wikipedia/commons/c/ce/PA260057.JPG',
+        title: 'Polish Cottage',
+        price: 34,
+        location: {
+            firstLine: 'no 23',
+            city: 'Gdansk',
+            code: 343903,
+            country: 'Poland'
+        },
+        contact: 'garydavis@hotmail.com',
+        available: false 
+    },
+    {
+        image: 'https://www.buyincolombia.com/wp-content/uploads/2018/08/buy-in-colombia-colombia-real-estate-realty-finca-raiz-colombia-lots-for-sale-lotes-en-venta-guatavita-gv-19-56-818x540.jpg',
+        title: 'London Flat',
+        price: 23,
+        location: {
+            firstLine: 'flat 15',
+            city: 'London',
+            code: 35433,
+            country: 'United Kingdom',
+        },
+        contact: 'andyluger@aol.com',
+        available: true
+    }
+]
 
-function showReviewTotal (value: number) {  
+
+function showReviewTotal (value: number, reviews : object, loyaltyUser : boolean) {  
     reviewTotalDisplay!.innerHTML = 'review total ' + value.toString() 
+    reviewTotalDisplay!.innerHTML += ` | last reviewed by: ${reviews[0].name}`
+    loyaltyUser ? reviewTotalDisplay!.innerHTML += `&#9733` : reviewTotalDisplay!.innerHTML += `` 
+    
 }
 
-showReviewTotal(reviews.length)
+showReviewTotal(reviews.length, reviews, reviews[0].loyaltyUser)
 
-function showMostRecentReviewerName (reviews){
-    reviewTotalDisplay!.innerHTML += ` ${reviews[0].name}`
+const you: {
+    firstName: string;
+    lastName: string;
+    isReturning: boolean;
+    age: number;
+    stayedAt: string[]
+} = {
+    firstName: 'Bobby',
+    lastName: 'Brown',
+    isReturning: true,
+    age: 35,
+    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
 
-showMostRecentReviewerName(reviews)
+
+function populateUser(isReturning : boolean, userName: string ) {
+    if (isReturning == true){
+        returningUserDisplay!.innerHTML = 'back'
+    }
+    userNameDisplay!.innerHTML = userName
+}
+
+populateUser(you.isReturning, you.firstName)
+
+const displayProperties =(properties)=>{
+    properties.forEach((e : object)=>{
+        propertyDashboard!.innerHTML += `<div class='single-property-container'><h2>${e['title']}</h2><img src=${e['image']} class='property-image'/></div>`
+    })
+}
+
+displayProperties(properties)
